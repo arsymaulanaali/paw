e90e1adf82ecd68f485ee4070d701dd3d77b6c32
arsymaulanaali <101117904+arsymaulanaali@users.noreply.github.com>
2022-06-17 21:53:24 +0700

    Add files via upload

---
+++ tugas2/hello.php
@@ -0,0 +1,6 @@
+<?php
+//memasukkan nama mahasiswa
+echo' Nama: arsy maulana ali ';
+//memasukkan Nim mahasiswa
+echo'Nim:200411100091';
+?> 
---
+++ tugas2/variable.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$angka1=45;
+$angka2=30;
+$angka3=10;
+
+$hasilpenjumlahan=$angka1+$angka2+$angka3;
+$hasilpengurangan=$angka1-$angka2-$angka3;
+$hasilperkalian=$angka1*$angka2*$angka3;
+echo"$hasilpenjumlahan";
+echo"<br>";
+echo"$hasilpengurangan";
+echo"<br>";
+echo"$hasilperkalian";
+?>
+</body>
+</html>
